0fdab568b9403eaf3fe76236c800845dd69bbbdb
Karim DRIDI <karim.dridi@gmail.com>
2021-04-07 10:44:42 +0200

    apply transformation

--- src/Transformer.hs
+++ src/Transformer.hs
@@ -4,11 +4,19 @@ module Transformer
     
 import Data.List (sort)
 
-import Pixel ( Pixel(..), Position(Position), ColorF, Image(..) )
+import Pixel
+    ( Pixel(..),
+      Position(Position),
+      Color(Color),
+      ColorF(..),
+      Image(..) )
 
 transformPixels :: [(ColorF, [Pixel])] -> Image
 transformPixels arr = Image w h p
   where
     w = let Pixel (Position _ j) _ = last p in j + 1
     h = let Pixel (Position i _) _ = last p in i + 1
-    p = sort $ concatMap snd arr
+    p = sort $ concatMap transform arr
+    transform (ColorF r g b, pixels) = map tr pixels
+      where
+        tr (Pixel position _) = Pixel position (Color (round r) (round g) (round b))
